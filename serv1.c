017f7f9982fdad24073bd03b6ddf95af4c41e6d6
sschwag <sschwag@okstate.edu>
2021-11-28 19:41:55 -0600

    Communicating Server Menus Between Server and Client

--- serv1.c
+++ serv1.c
@@ -1,19 +1,15 @@
-/*Sagar Sawant 
-*Group G
-*A11524117
-*sagar.sawant@okstate.edu
-*Description: This is a basic server program that will enable 
-*the program to connect with a multiple clients for basic TCP connection
-*
-**/
-
 #define MAXLINE 1024
 #include "handler.c"
 struct serv_info * info_s;
 
 
-
+// Driver code
 int main(int argc, char *argv[]) {
+
+        //this command line will give us number of servers to run in parallel
+        //this command line will also tell us about maximum number of threads
+        //each server thread will handle
+        //it will also tell about starting port
 
         if(argc < 4){
                 printf("Format is Server.out [starting port] [serv_count] [num_th]\n");
@@ -21,15 +17,21 @@ int main(int argc, char *argv[]) {
         }
 
         int port_1 = atoi(argv[1]);
+
         int serv_count =  atoi(argv[2]);
+
         info_s = (struct serv_info *)malloc(sizeof(struct serv_info) * serv_count);
+
         int num_th = atoi(argv[3]);
+
         pthread_t * t = (pthread_t *)malloc(sizeof(pthread_t) * serv_count);
  
  for(int i = 0; i < serv_count; i++){
 
                 info_s[i].port = port_1++;
+
                 info_s[i].num_th = num_th;
+
                 pthread_create(&t[i], NULL, handler_serv, (void *)&info_s[i]);
         }
 
--- servermenu.c
+++ servermenu.c
@@ -1,10 +1,13 @@
 //Written by: Scott Schwager
+//A20151349
 //sschwag@okstate.edu
 //Group G
 
+//#include "global.h"
 #include <stdio.h>
+#include <string.h>
 
-void buyerOrSeller() {
+void buyerOrSeller(int soc_conn) {
 	
 	//This function determines the role of each user
 	//Then it calls getUserInfo() with the role as an argument
@@ -16,22 +19,30 @@ void buyerOrSeller() {
 	//signal to client that server expects input or assume that client will provide write after every time it reads?
 	//separate funtion to updateUserInfo or reuse getUserInfo?
 	
+	char msg[1024];
 	int option;
 	
-	printf("Please choose your role:\n1. Buyer\n2. Seller\n");
-	scanf("%d", &option);
+	bzero(msg, sizeof(msg));
+	strcat(msg, "Please choose your role:\n1. Buyer\n2. Seller\n");
+	write(soc_conn, msg, sizeof(msg));
+	write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+	bzero(msg, sizeof(msg));
+	read(soc_conn, msg, sizeof(msg));
+	option = atoi(msg);
 	
 	if (option == 1) {
 		printf("Buyer\n");
-		getUserInfo("Buyer");
+		getUserInfo("Buyer", soc_conn);
+		showBuyerMenu(soc_conn);
 	} else {
 		printf("Seller\n");
-		getUserInfo("Seller");
-		showSellerMenu();
+		getUserInfo("Seller", soc_conn);
+		showSellerMenu(soc_conn);
 	}
+	return;
 }
 
-void getUserInfo(char userRole[6]) {
+void getUserInfo(char userRole[6], int soc_conn) {
 	
 	//This function gets the user information
 	//arguments: userRole (either "Buyer" or "Seller")
@@ -39,58 +50,201 @@ void getUserInfo(char userRole[6]) {
 	//The information that is gathered is the same regardless of the user's role
 	//This function is called by buyerOrSeller()
 	
+	char msg[1024];
 	char userId[50];
 	char userName[50];
 	char userPhone[12];
 	char userAddress[50];
 	
-	printf("ID:\n");
-	scanf("%s", &userId);
-	printf("Name:\n");
-	scanf("%s", &userName);
-	printf("Phone XXX-XXX-XXXX:\n");
-	scanf("%s", &userPhone);
-	printf("Address:\n");
-	scanf("%s", &userAddress);
+	//Get userId
+	bzero(msg, sizeof(msg));
+	strcat(msg, "ID:\n");
+	write(soc_conn, msg, sizeof(msg));
+	write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+	bzero(msg, sizeof(msg));
+	read(soc_conn, msg, sizeof(msg));
+	strcpy(userId, msg);
+	
+	//Get userName
+	bzero(msg, sizeof(msg));
+	strcat(msg, "Name:\n");
+	write(soc_conn, msg, sizeof(msg));
+	write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+	bzero(msg, sizeof(msg));
+	read(soc_conn, msg, sizeof(msg));
+	strcpy(userName, msg);
+	
+	//Get userPhone
+	bzero(msg, sizeof(msg));
+	strcat(msg, "Phone XXX-XXX-XXXX:\n");
+	write(soc_conn, msg, sizeof(msg));
+	write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+	bzero(msg, sizeof(msg));
+	read(soc_conn, msg, sizeof(msg));
+	strcpy(userPhone, msg);
+	
+	//Get userAddress
+	bzero(msg, sizeof(msg));
+	strcat(msg, "Address:\n");
+	write(soc_conn, msg, sizeof(msg));
+	write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+	bzero(msg, sizeof(msg));
+	read(soc_conn, msg, sizeof(msg));
+	strcpy(userAddress, msg);
+	
+	return;
 	
 	//Likely will put this info into a struct of some sort
 	//This information is then entered into the sellerInformation or customerInformation database depending on the userRole (Buyer or Seller)
 }
 
-void showSellerMenu() {
+void showSellerMenu(int soc_conn) {
 	
 	//This function shows the available options to sellers and then calls the functions for those options
+	//This function is called by buyerOrSeller()
+	
+	int option;
+	char msg[1024];
+	int done = 0;
+	
+	while (done != 1) {
+		bzero(msg, sizeof(msg));
+		strcat(msg, "\nWhat to you want to do?\n");
+		strcat(msg, "1. Update User Information\n");
+		strcat(msg, "2. Add a new product\n");
+		strcat(msg, "3. Remove an existing product\n");
+		strcat(msg, "4. Update the quantity of a product\n");
+		strcat(msg, "5. Update the price of a product\n");
+		strcat(msg, "6. View available products\n");
+		strcat(msg, "7. View orders for your products\n");
+		strcat(msg, "8. Exit the program\n");
+		
+		write(soc_conn, msg, sizeof(msg));
+		write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+		bzero(msg, sizeof(msg));
+		read(soc_conn, msg, sizeof(msg));
+		switch(atoi(msg)) {
+			case 1:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "Please Enter Updated User Information:\n");
+				write(soc_conn, msg, sizeof(msg));
+				write(soc_conn, "input", sizeof("input"));
+				bzero(msg, sizeof(msg));
+				read(soc_conn, msg, sizeof(msg));
+				printf("%s\n", msg);
+				break;
+			case 2:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "Please enter product information:\n");
+				write(soc_conn, msg, sizeof(msg));
+				write(soc_conn, "input", sizeof("input"));
+				bzero(msg, sizeof(msg));
+				read(soc_conn, msg, sizeof(msg));
+				printf("%s\n", msg);
+				break;
+			case 3:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "Product ID of product to be removed:\n");
+				write(soc_conn, msg, sizeof(msg));
+				write(soc_conn, "input", sizeof("input"));
+				bzero(msg, sizeof(msg));
+				read(soc_conn, msg, sizeof(msg));
+				printf("%s\n", msg);
+				break;
+			case 4:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "Product ID of product to be updated quantity:\n");
+				write(soc_conn, msg, sizeof(msg));
+				write(soc_conn, "input", sizeof("input"));
+				bzero(msg, sizeof(msg));
+				read(soc_conn, msg, sizeof(msg));
+				printf("%s\n", msg);
+				break;
+			case 5:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "Product ID of product to be updated price:\n");
+				write(soc_conn, msg, sizeof(msg));
+				write(soc_conn, "input", sizeof("input"));
+				bzero(msg, sizeof(msg));
+				read(soc_conn, msg, sizeof(msg));
+				printf("%s\n", msg);
+				break;
+			case 6:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "These are the products that you offer:\n");
+				write(soc_conn, msg, sizeof(msg));
+				break;
+			case 7:
+				bzero(msg, sizeof(msg));
+				strcat(msg, "These are the orders for your products:\n");
+				write(soc_conn, msg, sizeof(msg));
+				break;
+			case 8:
+				write(soc_conn, "exit", sizeof("exit"));
+				done = 1;
+				break;
+		}
+	}
+	return;
+}
+
+void showBuyerMenu(int soc_conn) {
+	
+	//This function shows the available options to buyers and then calls the functions for those options
 	//This function is called by buyerOrSeller()
 	
 	//Likely will loop this menu so a user can do multiple actions consecutively
 	
 	int option;
+	char msg[1024];
+	int done = 0;
 	
-	printf("\nWhat to you want to do?\n");
-	printf("1. Update User Information\n");
-	printf("2. Add a new product\n");
-	printf("3. Remove an existing product\n");
-	printf("4. Update the quantity of a product\n");
-	printf("5. Update the price of a product\n");
-	printf("6. View available products\n");
-	printf("7. View orders for your products\n");
-	
-	scanf("%d", &option);
-	switch(option) {
-		case 1:
-			break;
-		case 2:
-			printf("Please enter product information:\n");
-			break;
-		case 3:
-			break;
+	while (done != 1) {
+		bzero(msg, sizeof(msg));
+		strcat(msg, "\nWhat to you want to do?\n");
+		strcat(msg, "1. Update User Information\n");
+		strcat(msg, "2. Purchase a product\n");
+		strcat(msg, "3. Return a product\n");
+		strcat(msg, "4. View available products\n");
+		strcat(msg, "5. View orders you have placed\n");
+		strcat(msg, "6. View your billing information\n");
+		strcat(msg, "7. Exit the program\n");
+		
+		write(soc_conn, msg, sizeof(msg));
+		write(soc_conn, "input", sizeof("input"));	//Ask for input from client
+		bzero(msg, sizeof(msg));
+		read(soc_conn, msg, sizeof(msg));
+		switch(atoi(msg)) {
+			case 1:
+				break;
+				printf("Please Enter Updated User Information:\n");
+			case 2:
+				printf("Product ID of product you want to buy:\n");
+				break;
+			case 3:
+				printf("Product ID of product to be returned:\n");
+				break;
+			case 4:
+				printf("These are the available products to buy with their prices:\n");
+				break;
+			case 5:
+				printf("These are the orders you have placed:\n");
+				break;
+			case 6:
+				printf("This is your billing information:\n");
+				break;
+			case 7:
+				write(soc_conn, "exit", sizeof("exit"));
+				done = 1;
+				break;
+		}
 	}
+	return;
 }
 
-
-int main() {
+/*int main() {
 	//This is the driver function for testing purposes.
 	//This code will be put into the server code, to be ran after the server accepts a client.
 	buyerOrSeller();
 	return 0;
-}
+}*/
