5fedc10c5a01f0136f3fab3a324351065d3d88a2
ICook <isabellcook001@gmail.com>
2021-11-30 19:27:07 -0600

    adding s to printfs in dat..Functions

--- databaseFunctions.c
+++ databaseFunctions.c
@@ -429,7 +429,7 @@ void viewProductsAvailable(){
     for(int i = 0; i < tableOfProducts->count; i++)
     {
         Product product = tableOfProducts->entries[i];
-        printf("Product ID: %d\nQuantity Available: %d\nPrice: %f\n\nDescription: %s",
+        sprintf("Product ID: %d\nQuantity Available: %d\nPrice: %f\n\nDescription: %s",
                 product.productID,
                 product.numAvailable,
                 product.price,
@@ -476,7 +476,7 @@ void viewOrdersForProducts(int productID, int soc_conn)
             Order order = tableOfOrders->entries[i]; // get product with billing information
             
             char msg[500];
-            printf(msg, "Order ID: %d\nQuantity Purchased: %d\nTotal Order Price: %f\nDelivery Address: %s\n",
+            sprintf(msg, "Order ID: %d\nQuantity Purchased: %d\nTotal Order Price: %f\nDelivery Address: %s\n",
                 order.orderID,
                 order.numPurchased,
                 order.totalPrice,
